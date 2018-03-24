9d05bc36e1aa112bf107d04d5d72479f200d3067
Jonathan Luk <jluk87@gmail.com>
2018-03-24 19:33:14 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -7,5 +7,13 @@ class Doctor
     @appointments = []
   end
 
+  def add_appointment(appointment)
+    self.appointments << appointment
+    appointment.doctor = self
+  end
+
+  def patients
+    self.appointments.collect {|appointment| appointment.patient}
+  end
 
 end
