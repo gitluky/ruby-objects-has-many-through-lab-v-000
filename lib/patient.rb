8137a8fd5deb36830d8158631d7e1f5f802c8473
Jonathan Luk <jluk87@gmail.com>
2018-03-24 19:37:04 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,18 @@
 class Patient
 
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    self.appointments << appointment
+
+  end
+
+  def doctors
+    self.appointments.collect {|appointment| appointment.doctor}
+  end
 end
