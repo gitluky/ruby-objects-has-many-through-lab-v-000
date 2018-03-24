1ff988f1efde444fbb576e8e1841dc40a08daa16
Jonathan Luk <jluk87@gmail.com>
2018-03-24 19:11:23 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,20 +1,4 @@
 class Doctor
-  attr_reader :name, :appointments
-
-    def initialize(name)
-      @name = name
-      @appointments = []
-
-    end
-
-    def add_appointment(appointment)
-      self.appointments << appointment
-      appointment.doctor = self
-    end
-
-    def patients
-      self.appointments.collect {|appointment| appointment.patient}
-    end
 
 
 
